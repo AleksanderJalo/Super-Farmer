9b86828cf234f7370f15c6f44661a0efe6f4692c
Aleksander Jałoszyński <aleksander.jaloszynski@gmail.com>
2023-06-03 16:17:47 +0200

    Logo Change

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ function App() {
   return (
     <div>
       <div className="farm w-full h-screen text-center flex flex-col justify-center items-center gap-28">
-        <div className="absolute object-cover bg-gradient-to-tr from-green-700 to-blue-700 mix-blend-overlay w-full h-full"></div>
+        <div className="absolute object-cover bg-gradient-to-t from-green-700 via-orange-800 to-blue-900 mix-blend-overlay w-full h-full"></div>
         <Logo />
         {!playClick && (
           <div className="flex justify-center">
-            <div className="flex flex-col gap-10 border-[7px] border-black p-8 wood z-20">
+            <div className="flex flex-col gap-10 border-[7px] border-black p-8 wood z-20 rounded-xl">
               <PlayButton onClick={playHandler} />
               <HtpButton />
             </div>
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,7 @@ import React from "react";
 import { ReactComponent as Pig } from "./pig.svg";
 const Logo = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-6 border-8 border-black p-4 bg-gradient-to-tr from-white to-blue-300 rounded-xl z-10">
+    <div className="flex flex-col justify-center items-center gap-6 border-8 border-black p-4 bg-gradient-to-t from-green-600 via-orange-600 to-blue-600 rounded-xl z-10">
       <div className="flex flex-col gap-4 items-center font-moonkids">
         <div className="text-transparent logo bg-clip-text bg-gradient-to-r from-yellow-400 to-yellow-600 text-8xl">
           Super
