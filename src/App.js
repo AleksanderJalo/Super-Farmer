fa2130e744aad73b78aed584a80fccbc94500c75
Aleksander Jałoszyński <aleksander.jaloszynski@gmail.com>
2023-06-03 17:36:38 +0200

    Changed Logo apperance + from object to png

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import PlayButton from "./components/PlayButton";
-import OptionsButton from './components/OptionsButton'
-import Logo from "./components/Logo";
+import OptionsButton from "./components/OptionsButton";
+import logo from "./images/logo.png";
 import HtpButton from "./components/HtpButton";
 import { useState } from "react";
 
@@ -13,17 +13,14 @@ function App() {
 
   return (
     <div>
-      <div className="farm w-full h-screen text-center flex flex-col justify-center items-center gap-28">
+      <div className="farm w-full h-screen text-center flex flex-col justify-center items-center gap-14">
         <div className="absolute object-cover bg-gradient-to-t from-green-700 via-orange-800 to-blue-900 mix-blend-overlay w-full h-full"></div>
-        <div className="h-1/2 z-20">
-          <Logo />
-        </div>
-
+        <img src={logo} alt="logo" className="z-20 h-[500px]" />
         {!playClick && (
           <div className="flex justify-center">
             <div className="flex flex-col  border-[7px] gap-3 border-black p-8 wood z-20 rounded-xl">
               <PlayButton onClick={playHandler} />
-              <OptionsButton/>
+              <OptionsButton />
               <HtpButton />
             </div>
           </div>
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,13 +2,13 @@ import React from "react";
 import { ReactComponent as Pig } from "./pig.svg";
 const Logo = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-6 border-8 border-black p-4 bg-gradient-to-t from-green-600 via-orange-600 to-blue-600 rounded-xl z-10">
+    <div className="flex flex-col rounded-full justify-center items-center gap-6 border-8 border-black p-6 w-[650px] h-[650px] bg-gradient-to-t from-green-600 via-orange-600 to-blue-600 z-10">
       <div className="flex flex-col gap-4 items-center font-moonkids">
         <div className="text-transparent logo bg-clip-text bg-gradient-to-r from-yellow-400 to-yellow-600 text-8xl">
           Super
         </div>
         <Pig />
-        <div className="text-transparent logo bg-clip-text bg-gradient-to-r from-yellow-600 to-yellow-400 text-8xl">
+        <div className="text-transparent logo bg-clip-text bg-gradient-to-r from-yellow-600 to-yellow-400 text-8xl pb-8">
           FARMER
         </div>
       </div>
