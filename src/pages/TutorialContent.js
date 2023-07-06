7340001e73964dc8a9a5ef28bf33881163a03f77
Aleksander Jałoszyński <aleksander.jaloszynski@gmail.com>
2023-07-06 18:49:34 +0200

    Small Error Fix

--- src/pages/TutorialContent.js
+++ src/pages/TutorialContent.js
@@ -3,7 +3,6 @@ import TutorialPage1 from "./tutorialPages/TutorialPage1";
 import TutorialPage2 from "./tutorialPages/TutorialPage2";
 import TutorialPage3 from "./tutorialPages/TutorialPage3";
 import TutorialPage4 from "./tutorialPages/TutorialPage4";
-import TutorialPage5 from "./tutorialPages/TutorialPage5";
 const TutorialContent = (props) => {
   return (
     <div className="text-4xl w-full flex flex-col items-center">
