b8a6e399dabc3a8ea70e513125b0015acd805300
Aleksander Jałoszyński <aleksander.jaloszynski@gmail.com>
2023-07-06 19:51:51 +0200

    Making better Dicer Handler

--- src/components/ActionWindow.js
+++ src/components/ActionWindow.js
@@ -4,13 +4,14 @@ import { usePlayer1Store } from "../stores/player1";
 import TradeAction from "./Actions/TradeAction";
 import stringToAnimal from "./Helpers/AnimalStringToObject";
 import AiTurn from "./AiTurn";
+import diceRollHandler from "./Helpers/diceRollHandler";
 const ActionWindow = () => {
   const cleanupAi = () => {
     setWin(null);
     setPhase("trade");
     setLeftSpin(null);
     setRightSpin(null);
-  }
+  };
   const {
     isHuman,
     leftSpin,
@@ -20,7 +21,11 @@ const ActionWindow = () => {
     setRightSpin,
     turn,
     nextTurn,
+    farm,
+    deleteAnimal,
   } = usePlayer1Store();
+  const [looseLeft, setLooseLeft] = useState(null);
+  const [looseRight, setLooseRight] = useState(null);
   const [phase, setPhase] = useState("trade");
 
   const [win, setWin] = useState(null);
@@ -38,8 +43,10 @@ const ActionWindow = () => {
 
   useEffect(() => {
     if (leftSpin[turn] !== null && rightSpin[turn] !== null) {
+      setLooseLeft(leftSpin[turn]);
+      setLooseRight(rightSpin[turn]);
       setTimeout(() => {
-        setPhase("end");
+        setPhase("afterRoll");
       }, 1000);
       if (leftSpin[turn] === rightSpin[turn]) {
         addAnimal(leftSpin[turn]);
@@ -48,51 +55,59 @@ const ActionWindow = () => {
       setLeftSpin(null);
       setRightSpin(null);
     }
+    if (phase === "afterRoll") {
+      console.log(diceRollHandler(looseLeft, looseRight, farm[turn]))
+      
+    }
   });
   return (
     <div>
-      {isHuman[turn] && <div className="text-black border-4 w-[500px] border-black bg-white text-3xl ">
-        <div className="w-full bg-green-600 px-4 py-3 text-white border-b-4 border-black">
-          {phase === "trade" && (
-            <div> Player {(turn % 4) + 1} : Trade Phase</div>
+      {isHuman[turn] && (
+        <div className="text-black border-4 w-[500px] border-black bg-white text-3xl ">
+          <div className="w-full bg-green-600 px-4 py-3 text-white border-b-4 border-black">
+            {phase === "trade" && (
+              <div> Player {(turn % 4) + 1} : Trade Phase</div>
+            )}
+            {(phase === "roll" || phase === "end") && (
+              <div> Player {(turn % 4) + 1} : Rolling Phase</div>
+            )}
+          </div>
+          {phase === "trade" && <TradeAction phaseHandler={phaseHandler} />}
+          {phase === "roll" && (
+            <div className="flex justify-center gap-10 mt-4 mb-2 p-4">
+              <SpinWheel isHuman={true} isLeft={true} />
+              <SpinWheel isHuman={true} isLeft={false} />
+            </div>
           )}
-          {(phase === "roll" || phase === "end") && (
-            <div> Player {(turn % 4) + 1} : Rolling Phase</div>
+
+          {phase === "end" && (
+            <div className="m-8 flex flex-col items-center">
+              <div>Congratulations you won </div>
+              <div className="p-2 mt-2 border-4 border-black bg-green-600">
+                {stringToAnimal(win, 100)}
+              </div>
+              <div
+                className="border-4 border-black rounded-lg p-2 bg-red-600 text-white hover:bg-red-500 cursor-pointer mt-6 "
+                onClick={cleanup}
+              >
+                End Turn
+              </div>
+            </div>
+          )}
+          {phase === "afterRoll" && !win && (
+            <div className="m-8 flex flex-col items-center">
+              <div>Sorry, You Lost! </div>
+              <div
+                className="border-4 border-black rounded-lg p-2 bg-red-600 text-white hover:bg-red-500 cursor-pointer mt-6 "
+                onClick={cleanup}
+              >
+                End Turn
+              </div>
+            </div>
           )}
         </div>
-        {phase === "trade" && <TradeAction phaseHandler={phaseHandler} />}
-        {phase === "roll" && (
-          <div className="flex justify-center gap-10 mt-4 mb-2 p-4">
-            <SpinWheel isHuman={true} isLeft={true} />
-            <SpinWheel isHuman={true} isLeft={false} />
-          </div>
-        )}
-
-        {phase === "end" && win && (
-          <div className="m-8 flex flex-col items-center">
-            <div>Congratulations you won </div>
-            <div className="p-2 mt-2 border-4 border-black bg-green-600">{stringToAnimal(win, 100)}</div>
-            <div
-              className="border-4 border-black rounded-lg p-2 bg-red-600 text-white hover:bg-red-500 cursor-pointer mt-6 "
-              onClick={cleanup}
-            >
-              End Turn
-            </div>
-          </div>
-        )}
-        {phase === "end" && !win && (
-          <div className="m-8 flex flex-col items-center">
-            <div>Sorry, You Lost! </div>
-            <div
-              className="border-4 border-black rounded-lg p-2 bg-red-600 text-white hover:bg-red-500 cursor-pointer mt-6 "
-              onClick={cleanup}
-            >
-              End Turn
-            </div>
-          </div>
-        )}
-      </div>}
-      {!isHuman[turn] && <AiTurn cleanup={cleanupAi} /> }
+      )}
+      {!isHuman[turn] && <AiTurn cleanup={cleanupAi} />}
     </div>
   );
 };
---
+++ src/components/Helpers/diceRollHandler.js
@@ -0,0 +1,78 @@
+import { usePlayer1Store } from "../../stores/player1";
+
+const diceRollHandler = (leftRoll, rightRoll, farm) => {
+  if (leftRoll === rightRoll) {
+    if (farm.includes(leftRoll)) {
+      const howMany = Math.floor(
+        (farm.filter((animal) => animal === leftRoll).length + 2) / 2
+      );
+      return { get: Array(howMany).fill(leftRoll) };
+    } else {
+      return { get: [leftRoll], loose: [] };
+    }
+  } else {
+    const finalObject = { get: [], loose: [] };
+    const goodAnimals = ["r", "s", "p", "c", "h"];
+    for (const goodAnimal of goodAnimals) {
+      if (leftRoll === goodAnimal) {
+        const howMany =
+        Math.floor(farm.filter((animal) => animal === leftRoll).length + 1) / 2
+        
+        if (howMany > 0) {
+          for (let i = 0; (i = 0 < howMany); i++) {
+            finalObject.get.push(leftRoll);
+          }
+        }
+      }
+      if (rightRoll === goodAnimal) {
+        const howMany =
+        Math.floor(farm.filter((animal) => animal === rightRoll).length + 1) / 2
+        if (howMany > 0) {
+          for (let i = 0; i < howMany; i++) {
+            finalObject.get.push(rightRoll);
+          }
+        }
+      }
+    }
+    if (leftRoll === "w") {
+      if (!farm.includes("bd")) {
+        const sheep =
+          farm.filter((animal) => animal === "s").length +
+          finalObject.get.filter((animal) => animal === "s").length;
+        const pig =
+          farm.filter((animal) => animal === "p").length +
+          finalObject.get.filter((animal) => animal === "p").length;
+        const cow =
+          farm.filter((animal) => animal === "c").length +
+          finalObject.get.filter((animal) => animal === "c").length;
+        if (sheep > 0) {
+          finalObject.loose.push(Array(sheep).fill("s"));
+        }
+        if (pig > 0) {
+          finalObject.loose.push(Array(sheep).fill("p"));
+        }
+        if (cow > 0) {
+          finalObject.loose.push(Array(sheep).fill("c"));
+        }
+      }
+    } else {
+      finalObject.loose.push("bd");
+    }
+
+    if (rightRoll === "f") {
+      if (!farm.includes("sd")) {
+        const rabbits =
+          farm.filter((animal) => animal === "r").length +
+          finalObject.get.filter((animal) => animal === "r").length -
+          1;
+        if (rabbits > 0) {
+          finalObject.loose.push(Array(rabbits).fill("r"));
+        }
+      } else {
+        finalObject.loose.push("sd");
+      }
+    }
+    return finalObject;
+  }
+};
+export default diceRollHandler;
